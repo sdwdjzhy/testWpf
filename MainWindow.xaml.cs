55eee6bf812593ae221123aa65f0dbcb3a730fa5
sdwdjzhy <sdwdjzhy@qq.com>
2022-01-23 10:42:24 +0800

    test

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿using HandyControl.Tools;
+using System.Windows;
+
+namespace test
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            ConfigHelper.Instance.SetLang("zh-cn");
+        }
+
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            HandyControl.Controls.MessageBox.Show("Ok");
+        }
+
+    }
+}
